0e6ae57040e392d0f804a91259c9fc7b04742f72
Nahue <nlcostamagna@gmail.com>
2025-01-11 11:37:25 -0300

    initial ec2 instance

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,10 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create a VPC
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2 // Default is all AZs in the region
+    });
+
+    // Create a security group
+    const securityGroup = new ec2.SecurityGroup(this, 'MySecurityGroup', {
+      vpc,
+      description: 'Allow ssh access to ec2 instances',
+      allowAllOutbound: true
+    });
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'allow ssh access from the world');
+
+    // Define user data script
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(
+      'yum update -y',
+      'yum install -y httpd',
+      'systemctl start httpd',
+      'systemctl enable httpd'
+    );
+
+    // Create an EC2 instance
+    new ec2.Instance(this, 'MyInstance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+      securityGroup,
+      userData
+    });
 
     // The code that defines your stack goes here
 
